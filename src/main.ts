6733a10df89e30e4e9c83bc3206681cf5271f9a2
Amin NAIRI <nairi_a16@myges.fr>
2020-07-01 11:56:22 +0200

    :sparkles: using a IIFE for the main function

--- src/main.ts
+++ src/main.ts
@@ -7,26 +7,6 @@ const twitter = new Twitter({
     access_token_secret: process.env.TWITTER_TOKEN_SECRET || ""
 });
 
-interface Tweet {
-    full_text: string;
-}
-
-interface SearchedTweets {
-    statuses: Tweet[];
-
-    search_metadata: {
-        completed_in: number,
-        max_id: number,
-        max_id_str: string,
-        next_results: string,
-        query: string,
-        refresh_url: string,
-        count: number,
-        since_id: number,
-        since_id_str: string
-    }
-}
-
 const parameters: Readonly<Record<string, string | number | boolean>> = Object.freeze({
     q: encodeURIComponent("#Popcorn"),
     lang: "fr",
@@ -36,39 +16,6 @@ const parameters: Readonly<Record<string, string | number | boolean>> = Object.f
     result_type: "mixed",
     tweet_mode: "extended"
 });
-
-const onTweetSearch = (error: any, tweets: any) => {
-    if (error) {
-        console.log(error);
-        console.log("Unable to fetch all tweets from the #popcorn hashtag.");
-        return;
-    }
-
-    // BASE CASE
-    if (tweets.statuses.length === 0) {
-        console.log("NO MORE STATUSES TO ANALYZE");
-        return;
-    }
-
-    console.log("FILTERING ONLY THE FULL_TEXT OF THE RECEIVED TWEETS...");
-    const receivedTweets: string[] = tweets.statuses.map(({full_text}: Tweet): string => full_text).filter((full_text: string): boolean => !full_text.startsWith("RT"));
-    console.log(receivedTweets);
-
-    const max_id: string | null = new URLSearchParams(tweets.search_metadata.next_results).get("max_id");
-
-    // SECOND BASE CASE
-    if (max_id === null) {
-        console.log("NO MORE MAX_ID FOR THE NEXT REQUEST");
-        return;
-    }
-
-    const nextParams = {...parameters, max_id};
-    console.log("NEXT REQUEST INCOMMING...");
-    twitter.get("search/tweets", nextParams, onTweetSearch);
-};
-
-// @ts-ignore
-//twitter.get("search/tweets", PARAMS, onTweetSearch);
 
 const searchTweets = (parameters: Readonly<Record<string, string | number | boolean>>): Promise<string[]> => {
     return new Promise((resolve, reject) => {
@@ -101,10 +48,9 @@ const searchTweets = (parameters: Readonly<Record<string, string | number | bool
     });
 };
 
-const main = async () => {
+(async () => {
     const searchedTweets = await searchTweets(parameters);
 
+    // ALL TWEEEEEEEEEEEEEEEEEEEEEEEEETS
     console.log(searchedTweets);
-};
-
-main();
+})();
