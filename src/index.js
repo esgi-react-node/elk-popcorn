b1b2dba929fcdd8fd8af6cd6966088e49c5312d4
Amin NAIRI <nairi_a16@myges.fr>
2020-07-01 23:21:55 +0200

    :memo: added more comments

--- src/index.js
+++ src/index.js
@@ -1,37 +1,61 @@
+// Various polyfills to use newer ECMAScript syntax in older browsers like async/await
 import "@babel/polyfill";
 
+/**
+ * Asynchronously load a script instead of blocking the HTML rendering
+ *
+ * @param {string} url
+ * @return {Promise<void>}
+ */
+const loadScript = url => new Promise(resolve => {
+    const script = document.createElement("script");
+
+    script.setAttribute("src", url);
+    script.setAttribute("async", true);
+
+    document.head.appendChild(script);
+
+    script.addEventListener("load", resolve);
+});
+
+// Asynchronous main function (thanks ECMAScript for NOT implementing top-level await...)
 async function main() {
-    console.log("GET http://localhost:8081/search");
+    await loadScript("https://cdn.anychart.com/releases/v8/js/anychart-base.min.js");
+    await loadScript("https://cdn.anychart.com/releases/v8/js/anychart-tag-cloud.min.js");
 
-    const response = await fetch("http://localhost:8081/search");
+    // Endpoint for analyzing and processing requested data from Tweets
+    const analyzeEndpoint = "http://localhost:8081/analyze";
 
-    const jsonResponse = await response.json();
-    const tokens = jsonResponse.data.tokens;
-    const normalizedTokens = tokens.map(({token}) => token);
-    const occurrences = normalizedTokens.reduce((occurrences, word) => ({...occurrences, [word]: occurrences[word] ? occurrences[word] + 1 : 1}), {});
-    const sortedOccurrences = Object.entries(occurrences).sort(([, x], [, y]) => y - x).slice(0, 200);
-    const data = sortedOccurrences.map(([x, value]) => ({x, value}));
+    try {
+        // Fetch data processed by the server for the chart
+        const response = await fetch("http://localhost:8081/analyze");
 
-    // create a tag (word) cloud chart
-    const chart = anychart.tagCloud(data);
+        // JSON response formatted for the chart
+        const {data} = await response.json();
 
-    // set a chart title
-    chart.title("Mots les plus utilisés dans les Tweets de #Popcorn");
+        // create a tag (word) cloud chart
+        const chart = anychart.tagCloud(data);
 
-    // set an array of angles at which the words will be laid out
-    chart.angles([0]);
+        // set a chart title
+        chart.title("Ressenti général sur l'échantillon de Tweets analysés pour l'émission #Popcorn");
 
-    // enable a color range (with category property in data)
-    chart.colorRange(false);
+        // set an array of angles at which the words will be laid out
+        chart.angles([0]);
 
-    // set the color range length
-    chart.colorRange().length('80%');
+        // enable a color range (with category property in data)
+        chart.colorRange(false);
 
-    // display the word cloud chart
-    chart.container("container");
-    chart.draw();
+        // set the color range length
+        chart.colorRange().length('80%');
+
+        // display the word cloud chart
+        chart.container("container");
+        chart.draw();
+    } catch (error) {
+        console.log(`An error occcured while fetching data from ${analyzeEndpoint}`);
+        console.error(error.message);
+    }
 }
 
-window.addEventListener("load", () => {
-    main();
-});
+// Waiting for the DOM to be loaded before trying to inserting the word cloud
+window.addEventListener("load", main);
