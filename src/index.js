f6a3e6f9765dbbc8f4a0a8f5f265f10389ae0008
Amin NAIRI <nairi_a16@myges.fr>
2020-07-01 20:39:58 +0200

    :sparkles: added the word cloud from a server call

--- src/index.js
+++ src/index.js
@@ -2,33 +2,15 @@ import "@babel/polyfill";
 
 async function main() {
     console.log("GET http://localhost:8081/search");
+
     const response = await fetch("http://localhost:8081/search");
 
-    if (!response.ok) {
-        throw new Error("Unable to fetch data. Did you forgot to ingest data?");
-    }
-
-    const {hits: {hits}} = await response.json();
-
-    console.log(hits);
-
-    const data = [
-        {"x": "Mandarin chinese", "value": 1090000000},
-        {"x": "English", "value": 983000000},
-        {"x": "Hindustani", "value": 544000000},
-        {"x": "Spanish", "value": 527000000},
-        {"x": "Arabic", "value": 422000000},
-        {"x": "Malay", "value": 281000000},
-        {"x": "Russian", "value": 267000000},
-        {"x": "Bengali", "value": 261000000},
-        {"x": "Portuguese", "value": 229000000},
-        {"x": "French", "value": 229000000},
-        {"x": "Hausa", "value": 150000000},
-        {"x": "Punjabi", "value": 148000000},
-        {"x": "Japanese", "value": 129000000},
-        {"x": "German", "value": 129000000},
-        {"x": "Persian", "value": 121000000}
-    ];
+    const jsonResponse = await response.json();
+    const tokens = jsonResponse.data.tokens;
+    const normalizedTokens = tokens.map(({token}) => token);
+    const occurrences = normalizedTokens.reduce((occurrences, word) => ({...occurrences, [word]: occurrences[word] ? occurrences[word] + 1 : 1}), {});
+    const sortedOccurrences = Object.entries(occurrences).sort(([, x], [, y]) => y - x).slice(0, 200);
+    const data = sortedOccurrences.map(([x, value]) => ({x, value}));
 
     // create a tag (word) cloud chart
     const chart = anychart.tagCloud(data);
