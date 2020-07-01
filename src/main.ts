24ca0e2dbc53889e3a8a66fbdccb8b6809da1633
Amin NAIRI <nairi_a16@myges.fr>
2020-07-01 11:23:33 +0200

    :sparkles: on dead ça chacal

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,12 @@ import Twitter from "twitter";
 import { Tweet, bulk } from "./gulp";
 
 const twitter = new Twitter({
-    consumer_key: "QX1uK8WIDChlv0BgYcEyMaufM",
-    consumer_secret: "men8hQhJspDQIkQ4jXyiCDh6UlRxUdR5e516K3I51QVyi726Ih",
-    access_token_key: "569315807-CmJSTENBb6dwcmD3LwXnooYj0l4QMwVvRzexLz3u",
-    access_token_secret: "j8QlPRoyDKfHw28FZZk0GhScHhIlunz0raEclLryneW6e"
+    consumer_key: process.env.TWITTER_API_KEY || "",
+    consumer_secret: process.env.TWITTER_API_SECRET_KEY || "",
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN || "",
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET || ""
 });
 
-const PARAMS = Object.freeze({
-    q: encodeURIComponent("#Popcorn"),
-    lang: "fr",
-    locale: "fr",
-    count: 100,
-    include_entities: false,
-    result_type: "mixed",
-    until: "2020-06-23",
-    tweet_mode: "extended"
-});
 
 interface SearchedTweets {
     statuses: Tweet[];
@@ -35,35 +25,73 @@ interface SearchedTweets {
     }
 }
 
-const onTweetSearch = (error: Error, tweets: SearchedTweets): void => {
+const parameters: Readonly<Record<string, string | number | boolean>> = Object.freeze({
+    q: encodeURIComponent("#Popcorn"),
+    lang: "fr",
+    locale: "fr",
+    count: 100,
+    include_entities: true,
+    result_type: "mixed",
+    tweet_mode: "extended"
+});
+
+const onTweetSearch = (error: any, tweets: any) => {
     if (error) {
+        console.log(error);
         console.log("Unable to fetch all tweets from the #popcorn hashtag.");
         return;
     }
 
-    console.log(tweets);
+    // BASE CASE
+    if (tweets.statuses.length === 0) {
+        console.log("NO MORE STATUSES TO ANALYZE");
+        return;
+    }
 
-    const urlSearchParams = new URLSearchParams(tweets.search_metadata.next_results);
-    const urlSearchParamsEntries: Record<string, string> = Object.fromEntries(urlSearchParams.entries());
-    const query = urlSearchParamsEntries.q.split(" ")[0];
+    console.log("FILTERING ONLY THE FULL_TEXT OF THE RECEIVED TWEETS...");
+    const receivedTweets: string[] = tweets.statuses.map(({full_text}: Tweet): string => full_text).filter((full_text: string): boolean => !full_text.startsWith("RT"));
+    console.log(receivedTweets);
 
-    console.log(urlSearchParamsEntries);
+    const max_id: string | null = new URLSearchParams(tweets.search_metadata.next_results).get("max_id");
 
-    console.log("OK !!!!!!!!!!!!!!!!! JE PASSE A LA SUITE");
+    // SECOND BASE CASE
+    if (max_id === null) {
+        console.log("NO MORE MAX_ID FOR THE NEXT REQUEST");
+        return;
+    }
 
-
-    /*
-    twitter.get("search/tweets", searchTweetsParams, (error: Error, tweets): void => {
-        if (error) {
-            console.log("Unable to fetch all tweets from the #popcorn hashtag.");
-            return;
-        }
-
-        console.log(tweets);
-
-    });
-    */
+    const nextParams = {...parameters, max_id};
+    console.log("NEXT REQUEST INCOMMING...");
+    twitter.get("search/tweets", nextParams, onTweetSearch);
 };
 
 // @ts-ignore
-twitter.get("search/tweets", PARAMS, onTweetSearch);
+//twitter.get("search/tweets", PARAMS, onTweetSearch);
+
+const searchTweets = (parameters: Readonly<Record<string, string | number | boolean>>): Promise<string[]> => {
+    return new Promise((resolve, reject) => {
+        twitter.get("search/tweets", parameters, async (error, tweets) => {
+            if (error) {
+                resolve([]);
+                return;
+            }
+
+            if (tweets.statuses.length === 0) {
+                resolve([]);
+                return;
+            }
+            
+            const texts = tweets.statuses.map(({full_text}: {full_text: string}) => full_text);
+
+            resolve(texts);
+        });
+    });
+};
+
+const main = async () => {
+    const searchedTweets = await searchTweets(parameters);
+
+    console.log(searchedTweets);
+};
+
+main();
