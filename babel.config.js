3233c25a93d5e026460f0a432c6db7ebd8271a99
Amin NAIRI <nairi_a16@myges.fr>
2020-07-02 14:43:06 +0200

    :memo: added some comments for the babel configuration

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,11 @@
+// Stricter mode for preventing using old JavaScript syntax and idioms
 "use strict";
 
+// Babel settings
 module.exports = {
+    // Presets for transpiling the code
     presets: [
+        // Translate newer (ES6+) into older JavaScript (ES5)
         "@babel/preset-env"
     ]
 };
